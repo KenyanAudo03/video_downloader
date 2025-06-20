7ec7ea817ba80f353807482b57e96d7679f4de11
Victor Owino <vo4685336@gmail.com>
2025-06-20 23:38:26 +0300

    Fix URL pattern for downloader and ensure trailing slash for consistency

--- video_downloader/urls.py
+++ video_downloader/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
-    path("downloader", include("downloader.urls")),
+    path("downloader/", include("downloader.urls")),
+    path("search/", include("search.urls")),
 ]
 
 if settings.DEBUG:
