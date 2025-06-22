e967523d823bfccd668c8eb7c140fef6122da8ae
Victor Owino <vo4685336@gmail.com>
2025-06-22 22:48:43 +0300

    Add SHARE_YOUTUBE_API_KEY setting and include 'share' app in INSTALLED_APPS

--- video_downloader/settings.py
+++ video_downloader/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "downloader",
     "search",
+    "share",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,4 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+SHARE_YOUTUBE_API_KEY = config('YOUTUBE_API_KEY1')
