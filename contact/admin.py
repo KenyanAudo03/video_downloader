fd4a57ffc3d79529f8a2f964c2c93673a6455d32
Victor Owino <vo4685336@gmail.com>
2025-06-25 17:56:11 +0300

    Update formatted_created_at and created_at_formatted methods to use local time for accurate date display

--- contact/admin.py
+++ contact/admin.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from django.utils.html import format_html
 from .models import Contact
 from django.utils.formats import date_format
+from django.utils.timezone import localtime
+
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
@@ -20,13 +22,13 @@ class ContactAdmin(admin.ModelAdmin):
     actions = ["mark_as_read", "mark_as_unread"]
 
     def formatted_created_at(self, obj):
-        return obj.created_at.strftime("%B %d, %Y %H:%M")
+        return localtime(obj.created_at).strftime("%B %d, %Y %H:%M")
 
     formatted_created_at.short_description = "Created At"
 
     # For readonly detail view
     def created_at_formatted(self, obj):
-        return obj.created_at.strftime("%B %d, %Y %H:%M")
+        return localtime(obj.created_at).strftime("%B %d, %Y %H:%M")
 
     created_at_formatted.short_description = "Created At"
 
@@ -56,9 +58,10 @@ class ContactAdmin(admin.ModelAdmin):
     mark_as_unread.short_description = "Mark selected messages as unread"
 
     fieldsets = (
-        ("Contact Information", {
-            "fields": ("name", "email", "created_at_formatted", "message", "is_read")
-        }),
+        (
+            "Contact Information",
+            {"fields": ("name", "email", "created_at_formatted", "message", "is_read")},
+        ),
     )
 
     def has_add_permission(self, request):
@@ -73,7 +76,7 @@ class ContactAdmin(admin.ModelAdmin):
     def has_view_permission(self, request, obj=None):
         return True
 
-    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+    def changeform_view(self, request, object_id=None, form_url="", extra_context=None):
         """Override to mark message as read when opened"""
         if object_id:
             try:
