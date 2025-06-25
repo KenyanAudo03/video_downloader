83f651f2bcf29fc7493ecd39405e25992df9f8b4
Victor Owino <vo4685336@gmail.com>
2025-06-25 16:08:08 +0300

    Add privacy policy and terms of service URLs to routing

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,9 @@ from share.views import shared_link
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("privacy_policy/", views.privacy_policy, name="privacy_policy"),
+    path("terms_of_service/", views.terms_of_service, name="terms_of_service"),
+    path("contact/", views.contact, name="contact"),
     path("load-videos/", views.load_videos, name="load_videos"),
-
     path("<str:video_id>/", shared_link, name="shared_link"),
 ]
