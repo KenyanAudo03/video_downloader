b34c03bb8ec2c51290ce876c157af8da48ddba92
Victor Owino <vo4685336@gmail.com>
2025-06-26 10:58:04 +0300

    Rename downloader URL path to media_grabber for improved clarity

--- video_downloader/urls.py
+++ video_downloader/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
-    path("downloader/", include("downloader.urls")),
+    path("media_grabber/", include("downloader.urls")),
     path("search/", include("search.urls")),
     path("share/", include("share.urls")),
     path("feedback/", include("feedback.urls")),
