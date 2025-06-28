89d66994f54426f9cb362930893ea81a9e56c005
Victor Owino <vo4685336@gmail.com>
2025-06-28 17:25:47 +0300

    Add URL routing for audio app with home, search, and suggestions paths

---
+++ audio/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "audio"
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("search/", views.search_youtube, name="search_youtube"),
+    path('suggestions/', views.get_suggestions, name='get_suggestions'),
+]
