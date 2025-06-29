0d5a2eb0315faede6975b45f2614dbb1923efa4e
Victor Owino <vo4685336@gmail.com>
2025-06-29 17:19:24 +0300

    Add video detail route to audio URL patterns

--- audio/urls.py
+++ audio/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("search/", views.search_youtube, name="search_youtube"),
     path('suggestions/', views.get_suggestions, name='get_suggestions'),
+    path('<str:video_id>/', views.video_detail, name='video_detail')
 ]
